1c63b334990f6d07d4ee0c74d172f183ecc623d3
Shehan Abeyrathne <shehan.abeyrathne96@gmail.com>
2022-07-08 13:27:49 +0530

    Update repository.js

--- src/http/repository.js
+++ src/http/repository.js
@@ -1,12 +1,23 @@
 import { useQuery } from 'react-query';
 import { ApiClient } from './api-client';
 
-export const useGetTodos = () => {
+export const useGetTodos = (noOfResults) => {
   return useQuery(
     'todos',
     () => {
       console.info('server request sent.');
-      return ApiClient.get('https://randomuser.me/api/?results=40');
+      return ApiClient.get(`https://randomuser.me/api/?results=${noOfResults}`);
+    },
+    { refetchOnWindowFocus: false }
+  );
+};
+
+export const useGetTodosTest = (noOfResults) => {
+  return useQuery(
+    ['todos', noOfResults],
+    () => {
+      console.info('server request sent.');
+      return ApiClient.get(`https://randomuser.me/api/?results=${noOfResults}`);
     },
     { refetchOnWindowFocus: false }
   );
