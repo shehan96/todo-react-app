77f838b6487cc67a7f9ab008118db13954c87591
Shehan Abeyrathne <shehan.abeyrathne96@gmail.com>
2022-07-09 09:58:54 +0530

    Update repository.js

--- src/http/repository.js
+++ src/http/repository.js
@@ -1,4 +1,4 @@
-import { useQuery } from 'react-query';
+import { useMutation, useQuery } from 'react-query';
 import { ApiClient } from './api-client';
 
 export const useGetTodos = (noOfResults) => {
@@ -12,13 +12,8 @@ export const useGetTodos = (noOfResults) => {
   );
 };
 
-export const useGetTodosTest = (noOfResults) => {
-  return useQuery(
-    ['todos', noOfResults],
-    () => {
-      console.info('server request sent.');
-      return ApiClient.get(`https://randomuser.me/api/?results=${noOfResults}`);
-    },
-    { refetchOnWindowFocus: false }
-  );
+export const useGetTodoMutation = () => {
+  return useMutation((noOfResults) => {
+    return ApiClient.get(`https://randomuser.me/api/?results=${noOfResults}`);
+  });
 };
