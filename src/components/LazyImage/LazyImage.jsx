c80b2f74ff69bb88b265684efa7a8ab079e3b433
Shehan Abeyrathne <shehan.abeyrathne96@gmail.com>
2022-07-11 11:09:27 +0530

    Update LazyImage.jsx

--- src/components/LazyImage/LazyImage.jsx
+++ src/components/LazyImage/LazyImage.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './LazyImage.css';
 import { useIntersectionObserver } from '../../hooks/hooks';
 
@@ -9,6 +9,37 @@ import { useIntersectionObserver } from '../../hooks/hooks';
 export const LazyImage = ({ src, alt, color = '#F3F5F7' }) => {
   const { intersectionRef, isIntersected } = useIntersectionObserver();
   const url = isIntersected ? src : '';
+  const [dimensions, setDimension] = useState({
+    width: 0,
+    height: 0,
+  });
+
+  const getDimentions = (url) => {
+    return new Promise((resolve, reject) => {
+      let img = new Image();
+      img.onload = () => resolve(img);
+      img.onerror = () => reject();
+      img.src = url;
+    });
+  };
+
+  useEffect(() => {
+    if (url != '') {
+      getDimentions(url)
+        .then((img) => {
+          setDimension({
+            width: img.width,
+            height: img.height,
+          });
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    }
+    else{
+      console.log('No image');
+    }
+  }, [isIntersected]);
 
   return (
     <div className='lazy-image'>
@@ -18,7 +49,7 @@ export const LazyImage = ({ src, alt, color = '#F3F5F7' }) => {
         style={{ backgroundImage: `url("${url}")` }}
         aria-label={alt}
       />
-      <svg xmlns='http://www.w3.org/2000/svg' version='1.1' viewBox='0 0 1 1'>
+      <svg xmlns='http://www.w3.org/2000/svg' version='1.1' viewBox={`0 0 ${dimensions.width} ${dimensions.height}`}>
         <rect fill={color} height='1' width='1' />
       </svg>
     </div>
